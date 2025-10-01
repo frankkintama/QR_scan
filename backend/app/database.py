a530bab4ed561dd60f4be3d40b9d0204664ecca9
frankkintama <83528715+frankkintama@users.noreply.github.com>
2025-10-01 17:36:02 +0900

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -12,7 +12,9 @@ MONGO_DB = os.getenv("MONGO_DB")
 
 
 async def init_db():
+    #Tạo client kết nối đến MongoDB, motor cho phép làm việc với MongoDB
     client = AsyncIOMotorClient(MONGO_URI)
+    #chọn database để dùng
     db = client[MONGO_DB]
     await init_beanie(database=db, document_models=[User])
     print(f"Connected to MongoDB at {MONGO_URI}, using database {MONGO_DB}")
